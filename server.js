e3bb02fd45d6c1707db1992f50dfc7a7a8009e3d
Kevin <kjwagner613@gmail.com>
2026-01-09 14:44:15 -0800

    update

--- server.js
+++ server.js
@@ -3,36 +3,43 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 
-// Path to your video directory on Z:
-// const videoDir = 'Y:\\xxx\\BrandNew\\';
-const videoDir = 'C:\\Users\\kevin\\Downloads\\movs';
+// const videoDir = "/mnt/e/phoenixFiles/45645645456";
+// const videoDir = "/mnt/i/a";
+// const videoDir = "/mnt/e/phoenixFiles/sorted";
+// const videoDir = "/mnt/j/phoenixFiles/x/5454564";
+// const videoDir = "/mnt/i/New folder";
+// const videoDir = "/mnt/j/phoenixFiles/X/AA";
+// const videoDir = "/mnt/j/phoenixFiles/X/X";
+// const videoDir = "/mnt/j/phoenixFiles/x/f";
+const videoDir = "/mnt/j/phoenixFiles/x/aa";
+// const videoDir = "/mnt/j/phoenixFiles/x/e";
+// const videoDir = "/mnt/j/phoenixFiles/x/d";
 
-// Serve the index.html file for the root URL
+
+
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Serve the /videos endpoint to return the list of video filenames
-app.get('/videos', (req, res) => {
-    fs.readdir(videoDir, (err, files) => {
-        if (err) {
-            console.error('Error reading video directory:', err);
-            res.status(500).send('Error reading video directory');
-            return;
-        }
-        // Filter only .mp4 files
-        const mp4Files = files.filter(file => file.endsWith('.mp4'));
-        res.json(mp4Files);
-    });
+// list videos
+app.get(['/videos', '/videos/'], (req, res) => {
+  if (!fs.existsSync(videoDir)) {
+    return res.status(500).send(`Video directory not found: ${videoDir}`);
+  }
+  fs.readdir(videoDir, (err, files) => {
+    if (err) return res.status(500).send('Error reading video directory');
+    res.json(files.filter(f => f.toLowerCase().endsWith('.mp4')));
+  });
 });
 
-// Serve video files statically
+// serve actual files at /videos/<filename>
 app.use('/videos', express.static(videoDir));
-app.use('/video', express.static(path.join(__dirname, 'video')));
 
-// Start the server
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Serving videos from: ${videoDir}`);
 });
